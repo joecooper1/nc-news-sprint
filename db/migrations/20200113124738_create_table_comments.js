8a87e2477b54d84bbcde7d4590f98697f373d55f
joecooper1 <joecooper2991@gmail.com>
2020-02-06 21:24:53 +0000

    delete cascade

--- db/migrations/20200113124738_create_table_comments.js
+++ db/migrations/20200113124738_create_table_comments.js
@@ -2,7 +2,10 @@ exports.up = function(knex) {
   return knex.schema.createTable("comments", commentsTable => {
     commentsTable.increments("comment_id").primary();
     commentsTable.string("author").references("users.username");
-    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable
+      .integer("article_id")
+      .references("articles.article_id")
+      .onDelete("CASCADE");
     commentsTable.string("body", [500]);
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
